5d21f913b5e45fa65bbff9a903f8520e1479b2a4
Jan Sudczak <jansudczak@gmail.com>
2018-12-02 21:27:29 +0100

    yes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import './App.scss';
-import Player from './components/Player/player';
-import CardStack from './components/CardStack/card_stack';
+import React, { Component } from "react";
+import "./App.scss";
+import Player from "./components/Player/player";
+import CardStack from "./components/CardStack/card_stack";
 
 const drawDecisionWeights = [
   1,
@@ -17,13 +17,14 @@ const drawDecisionWeights = [
   1,
   1,
   0.8, //13
-  0.5,
+  0.5, //14
   0.35, //15
   0.2, //16
   0.1, //17
   0.08, //18
   0.05, //19
   0.03, //20
+  0 // 21
 ];
 class App extends Component {
   state = {
@@ -34,22 +35,30 @@ class App extends Component {
     playerScore: 0,
     cardsOnStack: [],
     amountOfDrawnCards: 2,
-    playerCanDraw: true,
-    isGameOn: false,
+    isPlayerActive: true
   };
   componentDidMount() {
     this.newGame();
   }
 
   componentDidUpdate(prevProps, prevState) {
-    prevState.isGameOn &&
-      this.checkWinner(prevState.isAIplaying, prevState.isPlayerPlaing) &&
-      this.setState({ isGameOn: false }) &&
-      alert(
-        this.checkWinner(prevState.isAIplaying, prevState.isPlayerPlaing) +
-          ' WINS!!',
-      );
+    if (!this.state.isPlayerPlaing && !this.state.isAIplaying) {
+      this.displayTheWinner();
+    }
   }
+
+  displayTheWinner = () => {
+    const playerPoints = this.getPoints("playerCards");
+    const AIpoints = this.getPoints("AICards");
+    let winner = "nobody";
+    if ((AIpoints <= 21 && AIpoints > playerPoints) || playerPoints > 21) {
+      winner = "AI";
+    } else {
+      winner = "Player";
+    }
+
+    console.warn("END OF GAME, ", winner, " won.");
+  };
 
   giveCards = (arrayName, amount) => {
     const stack = this.state.cardsOnStack;
@@ -64,14 +73,33 @@ class App extends Component {
     for (let i = 0; i < amount; i++) {
       givenCards = [
         ...givenCards,
-        stack.splice(this.getRandomInt(stack.length), 1)[0], //get random card from the stack and remove it from the stack
+        stack.splice(this.getRandomInt(stack.length), 1)[0] //get random card from the stack and remove it from the stack
       ];
     }
 
-    this.setState(oldState => ({
-      cardsOnStack: stack, //a copy of the stack without some cards
-      [arrayName]: [...oldState[arrayName], ...givenCards], //simply: old cards + new cards
-    }));
+    this.setState(
+      oldState => ({
+        cardsOnStack: stack, //a copy of the stack without some cards
+        [arrayName]: [...oldState[arrayName], ...givenCards] //simply: old cards + new cards
+      }),
+      () => {
+        console.log(
+          "Player: ",
+          this.getPoints("playerCards"),
+          "AI: ",
+          this.getPoints("AICards")
+        );
+        if (this.getPoints("AICards") > 21) {
+          console.log("AI has >= 21 points, PLAYER WON");
+          this.setState({ isAIplaying: false });
+        }
+
+        if (this.getPoints("playerCards") > 21) {
+          console.log("Player has >= 21 points, AI WON");
+          this.setState({ isPlayerPlaing: false });
+        }
+      }
+    );
   };
 
   getRandomInt(max) {
@@ -88,14 +116,13 @@ class App extends Component {
     this.setState({
       cardsOnStack: newCardSet,
       amountOfDrawnCards: 2,
-      playerCanDraw: true,
+      isPlayerActive: true,
       isAIplaying: true,
-      isPlayerPlaing: true,
-      isGameOn: true,
+      isPlayerPlaing: true
     });
   }
   shouldAIdraw = () => {
-    return Math.random() <= drawDecisionWeights[this.getPoints('AICards')];
+    return Math.random() <= drawDecisionWeights[this.getPoints("AICards")];
   };
 
   getPoints = cardArray => {
@@ -107,53 +134,54 @@ class App extends Component {
     return i;
   };
 
-  checkWinner = (isAIplaying, isPlayerPlaing) => {
-    const playerPoints = this.getPoints('playerCards');
-    const AIpoints = this.getPoints('AICards');
-    if (
-      playerPoints > 21 ||
-      (!isPlayerPlaing && AIpoints <= 21 && playerPoints < AIpoints)
-    )
-      return 'AI';
-    if (
-      AIpoints > 21 ||
-      (!isAIplaying && playerPoints <= 21 && playerPoints > AIpoints)
-    )
-      return 'player';
+  AIdoMove = () => {
+    if (this.shouldAIdraw()) {
+      this.giveCards("AICards", 1);
 
-    return null;
-  };
-
-  AIdrawCard = () => {
-    if (!this.shouldAIdraw()) {
-      //AI decided to pass
-      console.warn('AI decided to pass');
-      this.setState({ isAIplaying: false });
+      setTimeout(() => {
+        this.AIdoMove();
+      }, 500);
     } else {
-      //AI decided to continue playing
-      this.giveCards('AICards', this.state.amountOfDrawnCards);
+      this.setState(prevState => ({
+        isAIplaying: false,
+        isPlayerActive: prevState.isPlayerPlaing
+      }));
+      console.log("AI decided not to draw");
     }
   };
+
   handleStackClick = () => {
-    if (!this.state.playerCanDraw) return;
-    this.setState({ playerCanDraw: false });
-    this.giveCards('playerCards', this.state.amountOfDrawnCards);
-    this.state.amountOfDrawnCards > 1 &&
-      this.setState({ amountOfDrawnCards: 1 });
-    setTimeout(() => {
-      this.AIdrawCard();
-      this.setState({ playerCanDraw: true });
-    }, 500);
+    if (!this.state.isPlayerActive) return;
+
+    this.giveCards("playerCards", this.state.amountOfDrawnCards);
+    if (this.state.amountOfDrawnCards > 1) {
+      this.setState({ amountOfDrawnCards: 1 }, () => {
+        setTimeout(() => {
+          this.setState({ isPlayerActive: false });
+          this.AIdoMove();
+        }, 500);
+      });
+    }
+  };
+
+  handleOwnCardsClick = () => {
+    if (!this.state.isPlayerActive) return;
+    console.log("clicked own cards");
+    this.setState({ isPlayerActive: false, isPlayerPlaing: false });
   };
   render() {
     return (
       <div className="App">
         <Player inverted={true} cards={this.state.AICards} />
         <CardStack
-          dimmed={!this.state.playerCanDraw}
+          dimmed={!this.state.isPlayerActive}
           onClick={this.handleStackClick}
         />
-        <Player cards={this.state.playerCards} />
+        <Player
+          dimmed={!this.state.isPlayerActive}
+          onClick={this.handleOwnCardsClick}
+          cards={this.state.playerCards}
+        />
       </div>
     );
   }
--- src/components/Player/player.js
+++ src/components/Player/player.js
@@ -5,9 +5,12 @@ import ClassNames from "classnames";
 
 class Player extends Component {
   render() {
-    const { cards, inverted } = this.props;
+    const { cards, inverted, onClick, dimmed } = this.props;
     return (
-      <div className={ClassNames("player__cards", { inverted })}>
+      <div
+        onClick={onClick}
+        className={ClassNames("player__cards", { inverted }, { dimmed })}
+      >
         {cards &&
           cards.length &&
           cards.map(card => (
