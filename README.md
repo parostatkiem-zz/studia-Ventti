f9cefe42dc640106ab73ebf670c77dbdae17007f
Sudczak, Jan <jan.sudczak@sap.com>
2019-01-21 12:57:58 +0100

    latest improvements

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # studia-Ventti
+## Zasady gry:
 http://my-summer-car.wikia.com/wiki/Ventti
+
+## Jak uruchomić projekt
+1. Upewnij się, ze na Twoim komputerze jest zainstalowany i działający [Node](https://nodejs.org/en/) w jednej z najnowszych wersji. Jeśli nie, zainstaluj go.
+2. Uruchom terminalu i przejdź w nim do folderu, w którym znajduje się plik który aktualnie czytasz.
+3. Wpisz następujący komendy:
+
+```bash
+npm install
+
+npm start
+```
+4. Powinna otworzyć się domyślna przeglądarka intenetowa z otwartą właściwą stroną. Jeśli tak się nie stało, uruchom ją manualnie i wprowadź adres `http://localhost:3000`
+
--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -1,9 +1,9 @@
-import React from "react";
-import "./Card.scss";
-import classNames from "classnames";
+import React from 'react';
+import './Card.scss';
+import classNames from 'classnames';
 
 const Card = ({ value, mark }) => (
-  <div className={classNames("card", { red: mark === 0 || mark === 3 })}>
+  <div className={classNames('card', { red: mark === 0 || mark === 3 })}>
     <span className="value">{GetValueSymbol(value)}</span>
     <span className="mark">{GetMarkSymbol(mark)}</span>
 
@@ -15,26 +15,26 @@ const Card = ({ value, mark }) => (
 );
 
 const GetValueSymbol = value => {
-  if (value === 1) return "A";
+  if (value === 1) return 'A';
   if (value >= 2 && value <= 10) {
     return value;
   }
-  if (value === 11) return "J";
-  if (value === 12) return "Q";
-  if (value === 13) return "K";
+  if (value === 11) return 'J';
+  if (value === 12) return 'Q';
+  if (value === 13) return 'K';
 };
 const GetMarkSymbol = mark => {
   switch (mark) {
     case 0:
-      return "[";
+      return '[';
     case 1:
-      return "]";
+      return ']';
     case 2:
-      return "{";
+      return '{';
     case 3:
-      return "}";
+      return '}';
     default:
-      return "[";
+      return '';
   }
 };
 
